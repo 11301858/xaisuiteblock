b2645fb20d3adc0c9f78eaf34407a1c36342f0d2
11301858 <66180831+11301858@users.noreply.github.com>
2023-03-31 18:44:40 -0700

    Update GeneratorStubs.js

--- scripts/GeneratorStubs.js
+++ scripts/GeneratorStubs.js
@@ -12,7 +12,7 @@ Blockly.Python['load_data'] = function(block) {
   var value_target = Blockly.Python.valueToCode(block, 'target', Blockly.Python.ORDER_ATOMIC);
   var value_cut = Blockly.Python.valueToCode(block, 'cut', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = '...';
+  var code = 'load_data_CSV(' + value_data + ', ' + value_target + ', ' + value_cut + ')';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
@@ -20,14 +20,14 @@ Blockly.Python['load_data'] = function(block) {
 Blockly.Python['compare_explanations'] = function(block) {
   var value_filenames = Blockly.Python.valueToCode(block, 'filenames', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = '...\n';
+  var code = 'compare_explanations(' + value_filenames + ')';
   return code;
 };
 
 Blockly.Python['model'] = function(block) {
   var value_model = Blockly.Python.valueToCode(block, 'model', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = '...';
+  var code = 'return ' + value_model ;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
@@ -35,7 +35,7 @@ Blockly.Python['model'] = function(block) {
 Blockly.Python['explainers'] = function(block) {
   var value_explainers = Blockly.Python.valueToCode(block, 'explainers', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = '...';
+  var code = 'return ' + value_explainers;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };

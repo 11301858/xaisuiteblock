2684691de42bacdac8cce439d1e6127cf71ad3ba
11301858 <66180831+11301858@users.noreply.github.com>
2023-08-15 16:27:03 -0700

    Update python.js

--- src/generators/python.js
+++ src/generators/python.js
@@ -7,7 +7,7 @@ python.pythonGenerator.forBlock['dataloader'] = function(block, generator) {
   var value_cut = generator.valueToCode(block, 'Cut', python.Order.ATOMIC);
   var value_categorical = generator.valueToCode(block, 'Categorical', python.Order.ATOMIC);
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = ('DataLoader(data = ' + value_data + ', source = ' + dropdown_sourcedropdown + ', type = ' + dropdown_typedropdown + ', variable_names = ' + value_variablenames + ', target_names = ' + value_target + ', cut = ' + value_cut + ', categorical = ' + value_categorical + ')');
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
@@ -15,7 +15,7 @@ python.pythonGenerator.forBlock['dataloader'] = function(block, generator) {
 python.pythonGenerator.forBlock['data_loader_simple'] = function(block, generator) {
   var value_data = generator.valueToCode(block, 'data', python.Order.ATOMIC);
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = ('DataLoader(data = ' + value_data + ')');
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
@@ -25,21 +25,21 @@ python.pythonGenerator.forBlock['dataprocessor'] = function(block, generator) {
   var number_test_size = block.getFieldValue('test_size');
   var text_transform = block.getFieldValue('transform');
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = ('DataProcessor(' + value_dataloader + ', ' + number_test_size + ', ' + text_transform + ')');
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
 
 python.pythonGenerator.forBlock['auto'] = function(block, generator) {
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = '"auto"';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
 
 python.pythonGenerator.forBlock['none'] = function(block, generator) {
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = 'None';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
@@ -51,7 +51,7 @@ python.pythonGenerator.forBlock['modeltrainer'] = function(block, generator) {
   var dropdown_task = block.getFieldValue('task');
   var value_explainers = generator.valueToCode(block, 'explainers', python.Order.ATOMIC);
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = ('ModelTrainer(' + value_model + ', ' + value_withdata + ', ' + dropdown_tasktype + ', ' + dropdown_task + ', ' + value_explainers + ')');
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
@@ -59,7 +59,7 @@ python.pythonGenerator.forBlock['modeltrainer'] = function(block, generator) {
 python.pythonGenerator.forBlock['getlocalexplanations'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'NAME', python.Order.ATOMIC);
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = value_name + '.getExplanationsFor([])';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
@@ -67,7 +67,7 @@ python.pythonGenerator.forBlock['getlocalexplanations'] = function(block, genera
 python.pythonGenerator.forBlock['getsummaryexplanations'] = function(block, generator) {
   var value_explanations = generator.valueToCode(block, 'explanations', python.Order.ATOMIC);
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = value_explanations + '.getSummaryExplanations()';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
@@ -75,7 +75,7 @@ python.pythonGenerator.forBlock['getsummaryexplanations'] = function(block, gene
 python.pythonGenerator.forBlock['insight_generator'] = function(block, generator) {
   var value_explanations = generator.valueToCode(block, 'explanations', python.Order.ATOMIC);
   // TODO: Assemble python into code variable.
-  var code = '...';
+  var code = 'InsightGenerator(' + value_explanations + ')';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.python.ORDER_NONE];
 };
@@ -85,6 +85,6 @@ python.pythonGenerator.forBlock['calculateexplainersimilarity'] = function(block
   var value_explainer1 = generator.valueToCode(block, 'explainer1', python.Order.ATOMIC);
   var value_explainer2 = generator.valueToCode(block, 'explainer2', python.Order.ATOMIC);
   // TODO: Assemble python into code variable.
-  var code = '...\n';
+  var code = value_insights + '.calculateExplainerSimilarity(' + value_explainer1 + ', ' + value_explainer2 + ')';
   return code;
 };
